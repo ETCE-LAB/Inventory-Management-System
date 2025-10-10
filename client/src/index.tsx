471a3a5eb3d647b4441200a9c4f8817e8b4d849d
Anant Sujatanagarjuna <anant.essen@gmail.com>
2025-10-10 18:25:03 +0200

    Fixed homepage

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -72,7 +72,7 @@ store.dispatch(setSelectedPage(1))
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename="/ims">
+      <BrowserRouter basename="/">
           <Provider store={store}>
               <MantineProvider theme={theme} defaultColorScheme="dark" >
                   <Notifications position="top-right"/>
